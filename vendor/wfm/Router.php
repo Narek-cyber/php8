a6c2312967ff7146713e2781fe817d92e6b95585
Narek Sahakyan <narsahakyan.work@gmail.com>
2026-01-25 23:18:43 +0400

    The Router Class. Part 2

--- vendor/wfm/Router.php
+++ vendor/wfm/Router.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Wfm;
-
 class Router
 {
     /**
@@ -45,6 +44,64 @@ class Router
      */
     public static function dispatch($url): void
     {
-        var_dump($url);
+        if (self::matchRoute($url)) {
+            echo 'OK';
+        } else {
+            echo 'NO';
+        }
+    }
+
+    /**
+     * @param $url
+     * @return bool
+     */
+    public static function matchRoute($url): bool
+    {
+        foreach (self::$routes as $pattern => $route) {
+            if (preg_match("#$pattern#", $url, $matches)) {
+                foreach ($matches as $k => $v) {
+                    if (is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+
+                if (empty($route['action'])) {
+                    $route['action'] = 'index';
+                }
+
+                if (!isset($route['admin_prefix'])) {
+                    $route['admin_prefix'] = '';
+                } else {
+                    $route['admin_prefix'] = '\\';
+                }
+
+                debug($route);
+                $route['controller'] = self::upperCamelCase($route['controller']);
+
+                debug($route);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * CamelCase
+     * @param $name
+     * @return string
+     */
+    protected static function upperCamelCase($name): string
+    {
+        return str_replace(' ', '', ucwords(str_replace('-', ' ', $name)));
+    }
+
+    /**
+     * camelCase
+     * @param $name
+     * @return string
+     */
+    protected static function lowerCamelCase($name): string
+    {
+        return lcfirst(self::upperCamelCase($name));
     }
 }
