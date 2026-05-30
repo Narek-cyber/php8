594fa3357cac1073d0f917d9e1a1b9ebbe63acb5
Narek Sahakyan <narsahakyan.work@gmail.com>
2026-05-30 19:58:48 +0400

    migration of the AdminLTE template. Part 1

---
+++ app/Controllers/Admin/AppController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+use Wfm\Controller;
+
+class AppController extends Controller
+{
+    public false|string $layout = 'admin';
+}
---
+++ app/Controllers/Admin/MainController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+class MainController extends AppController
+{
+    public function indexAction()
+    {
+
+    }
+}
